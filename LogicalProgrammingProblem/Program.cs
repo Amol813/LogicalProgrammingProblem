71de464e62979fbb66035481380ad219b44ab6a6
unknown <amoljadhav>
2023-03-09 14:03:49 +0530

    [amol]add:Welcome Logical Programming

---
+++ LogicalProgrammingProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogicalProgrammingProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Logical Programming!");
+        }
+    }
+}
