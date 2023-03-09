9bce7c8abd3b4bc98e8385402ed847528a7215d9
unknown <amoljadhav>
2023-03-09 15:05:54 +0530

    [amol]add:FibonacciSeries

--- LogicalProgrammingProblem/Program.cs
+++ LogicalProgrammingProblem/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace LogicalProgrammingProblem
+﻿using LogicalProgrammingPracticeProblem;
+using System;
+
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Welcome to Logical Programming Practice Problem!");
+        bool flag = true;
+        while (flag)
         {
-            Console.WriteLine("Welcome To The Logical Programming!");
+            Console.Write("1. Fibonacci\n2. Exit\nEnter your option to execute: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FibonacciSeries fib = new FibonacciSeries();
+                    Console.Write("Enter the number of Fibonacci Series: ");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    fib.PrintFibonacciSeries(num);
+                    break;
+                case 2:
+                    flag = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
         }
     }
 }
